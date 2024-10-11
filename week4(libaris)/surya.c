db48b6cd0e661ace6cbfcdaae86fcb33532c4d6e
Rushikesh1432 <rushikeshchilimikoti@gmail.com>
2024-10-11 13:01:18 +0530

    hi

---
+++ week4(libaris)/surya.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void hi(){
+  printf("Rushikesh is Erri pook saale");
+}
+int main(){
+    hi();
+}
--- week5(unit test)/textcal.py
+++ week5(unit test)/textcal.py
@@ -1,8 +1,26 @@
 import pytest
 from calculator import square
 
-def test_square():
+
+#defining more  test cases helps to find the mistake we did
+ 
+#pytest runs all test cases even one test fail
+
+
+def test_positive():
     assert square(2)==4
     assert square(3)==9
+
+
+
+def test_negative():    
     assert square(-2)==4
-    assert square(8)==64
+    assert square(-8)==64
+
+def test_zero():
+    assert square(0)==0
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
+        #This code 
