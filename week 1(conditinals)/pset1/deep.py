c6b54de85a93db3bce81236d55e3e87c710d1f80
Rushikesh1432 <rushikeshchilimikoti@gmail.com>
2024-10-11 12:42:57 +0530

    hi

--- week 1(conditinals)/pset1/deep.py
+++ week 1(conditinals)/pset1/deep.py
@@ -1,3 +1,6 @@
+#In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything,
+# outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
+
 x = str(input("What is the to the great question of Life, the universe, and Everything? ")).strip().lower()
 
 match x:
