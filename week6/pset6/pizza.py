cb99f14f3d95fe2e1bddf9db4ca8485ce83df56e
Rushikesh1432 <rushikeshchilimikoti@gmail.com>
2025-02-10 13:46:48 +0530

    hiiiiii

--- week6/pset6/pizza.py
+++ week6/pset6/pizza.py
@@ -1,9 +1,24 @@
 from tabulate import tabulate
-list=[]
-with open("regular1.csv","r") as file:
-    for line in file:
-     PIZZA,LARGE,SMALL=line.rstrip().split(",")
-     sub=[PIZZA,LARGE,SMALL]
-     list.append(sub)
+import sys
 
+if len(sys.argv)>2:
+  sys.exit("Too many arguments")
+elif len(sys.argv)<2:
+  sys.exit("Too few arguments")
+
+try:
+   file=open(sys.argv[1],"r")
+except FileNotFoundError:
+  sys.exit("File not found")
+
+if not sys.argv[1].endswith(".csv"):
+   sys.exit("File format error")
+
+list=[]
+
+for line in file:
+    PIZZA,LARGE,SMALL=line.rstrip().split(",")
+    sub=[PIZZA,LARGE,SMALL]
+    list.append(sub)
+    
 print(tabulate(list,headers="firstrow",tablefmt="grid"))
