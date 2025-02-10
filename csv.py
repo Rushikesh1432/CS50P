1c58dd3efd3d8564884af33c5271a5fc2d977753
Rushikesh1432 <rushikeshchilimikoti@gmail.com>
2025-02-10 13:48:36 +0530

    hello

--- csv.py
+++
@@ -1,8 +0,0 @@
-list=[]
-with open("hi.csv","r") as file:
-    for line in file:
-        name,place=line.strip().split(",")
-         print(f"{name}:{place}")
-        list.append(f"{name}:{place}")
-for i in sorted(list):
-    print(f"{i}")
--- read.py
+++
@@ -1,5 +0,0 @@
-
-with open("hi.txt","r") as file:
-    lines=file.readlines()
-for line in sorted(lines):
-    print(f"{line.strip()}")
